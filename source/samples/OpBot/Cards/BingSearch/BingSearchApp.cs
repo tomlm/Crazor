fbd6aa402fb06384b1eea2fa41f1c754b80bed87
Tom Laird-McConnell <tomlm@microsoft.com>
2022-09-14 21:40:04 -0700

    add bingsearch

---
+++ source/samples/OpBot/Cards/BingSearch/BingSearchApp.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Bot.Cards;
+
+namespace OpBot.Cards.BingSearch
+{
+    public class BingSearchApp: CardApp
+    {
+        public BingSearchApp(IServiceProvider services)
+            : base(services)
+        {
+        }
+    }
+}
---
+++ source/samples/OpBot/Cards/BingSearch/Default.cshtml.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Bing.WebSearch.Models;
+using Microsoft.Bing.WebSearch;
+using System.Reflection;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace OpBot.Cards.BingSearch
+{
+    public class DefaultModel
+    {
+        public int Offset { get; set; }
+
+        public IList<WebPage> Results { get; set; } = new List<WebPage>();
+
+        public bool HasNext => Results.Any(); 
+
+        public bool HasPrev => Results.Any() && Offset > 0;
+    }
+}
