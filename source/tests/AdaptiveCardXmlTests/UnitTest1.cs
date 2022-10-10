8140a0bb3cc8ae88b7cd831fc76c312134c51461
Tom Laird-McConnell <tomlm@microsoft.com>
2022-10-10 05:09:05 -0700

    adding unit tests

---
+++ source/tests/AdaptiveCardXmlTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using AdaptiveCards;
+using Newtonsoft.Json;
+
+namespace AdaptiveCardXmlTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
