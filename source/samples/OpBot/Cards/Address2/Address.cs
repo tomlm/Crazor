bbf38083843723e1bca184b93ea27d8bdbab0597
Tom Laird-McConnell <tomlm@microsoft.com>
2022-10-15 16:24:21 -0700

    update comments

--- source/samples/OpBot/Cards/Address2/Address.cs
+++ source/samples/OpBot/Cards/Address2/Address.cs
@@ -8,6 +8,21 @@ namespace OpBot.Cards.Address2
     public class Address
     {
         public string? Id { get; set; } = Utils.GetNewId();
+        //Supported Data Annotations 
+        //  [PasswordPropertyText]
+        //  [Range]
+        //  [Phone]
+        //  [Url]
+        //  [Email]
+        //  [StringLength]
+        //  [PasswordPropertyText]
+        //  [MaxLength]
+        //  [RegularExpression("Ames", ErrorMessage = "The value must be Ames")]
+        //  [DataType(DataType.Text)]
+        //  [DataType(DataType.Email)]
+        //  [DataType(DataType.Password)]
+        //  [DataType(DataType.Tel)]
+        //  [DataType(DataType.Url)]
 
         [Required]
         [StringLength(100)]
