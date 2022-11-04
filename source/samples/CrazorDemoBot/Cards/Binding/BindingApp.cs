79b87bf7819d63aae191c17859bd6e30e474bb85
Tom Laird-McConnell <tomlm@microsoft.com>
2022-11-04 15:17:04 -0700

    add binding example

---
+++ source/samples/CrazorDemoBot/Cards/Binding/BindingApp.cs
@@ -0,0 +1,12 @@
+﻿using Crazor;
+
+namespace CrazorDemoBot.Cards.Binding
+{
+    public class BindingApp : CardApp
+    {
+        public BindingApp(IServiceProvider services)
+            : base(services)
+        {
+        }
+    }
+}
