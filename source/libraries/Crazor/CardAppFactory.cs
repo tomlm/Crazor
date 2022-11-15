7190aa42cd274c661a182bfcb81330114b009850
Tom Laird-McConnell <tomlm@microsoft.com>
2022-11-14 18:19:18 -0800

    update cardfactory

--- source/libraries/Crazor/CardAppFactory.cs
+++ source/libraries/Crazor/CardAppFactory.cs
@@ -5,19 +5,21 @@ namespace Crazor
 {
     public class CardAppFactory
     {
-        private IServiceByNameFactory<CardApp> _cardAppFactory;
+        private IServiceByNameFactory<CardApp> _cardAppByName;
 
         public CardAppFactory(IServiceByNameFactory<CardApp> serviceByName)
         {
-            _cardAppFactory = serviceByName;
+            _cardAppByName = serviceByName;
         }
+
+        public IEnumerable<string> GetNames() => _cardAppByName.GetNames().OrderBy(n => n);
 
         public CardApp Create(string name)
         {
-            var cardName = _cardAppFactory.GetNames().SingleOrDefault(cardName => String.Equals(cardName, name, StringComparison.OrdinalIgnoreCase));
+            var cardName = _cardAppByName.GetNames().SingleOrDefault(cardName => String.Equals(cardName, name, StringComparison.OrdinalIgnoreCase));
             if (cardName != null)
             {
-                var cardApp = _cardAppFactory.GetByName(cardName);
+                var cardApp = _cardAppByName.GetByName(cardName);
                 if (cardApp.GetType() == typeof(CardApp))
                 {
                     cardApp.Name = cardName;
