a175d39d7d6906c54071dbf131ed9f45386c6b96
Tom Laird-McConnell <tomlm@microsoft.com>
2022-12-01 13:46:36 -0800

    add tabmoudle unit test

--- source/tests/CrazorTests/CardActivityHandlerTests.cs
+++ source/tests/CrazorTests/CardActivityHandlerTests.cs
@@ -176,5 +176,48 @@ namespace CrazorTests
             Assert.AreEqual(200, response.Status);
             Assert.IsNull(response.Body);
         }
+
+
+        [TestMethod]
+        public async Task TestTabModule()
+        {
+            var bot = Services.GetService<IBot>();
+            var configuration = Services.GetService<IConfiguration>();
+            var hostUri = configuration.GetValue<string>("HostUri");
+            var adapter = new CardTestAdapter(bot);
+
+            var invokeResponse = await adapter.Invoke(CreateTabFetchTaskActivity(new TaskModuleRequest()
+            {
+                TabEntityContext = new TabEntityContext() { TabEntityId = "/Cards/TabModule" }
+            }));
+            var tabResponse = ObjectPath.MapValueTo<TabResponse>(invokeResponse.Body);
+            Assert.IsNotNull(tabResponse);
+            Assert.IsNotNull(tabResponse.Tab);
+            Assert.AreEqual("continue", tabResponse.Tab.Type);
+            Assert.IsNotNull(tabResponse.Tab.Value);
+            var card = ObjectPath.MapValueTo<AdaptiveCard>(tabResponse.Tab.Value.Cards.Single().Card);
+
+            card.AssertTextBlock("Counter: 0")
+                .AssertHasNoRefresh()
+                .AssertHasSession()
+                .AssertHasOnlySubmitActions();
+
+            invokeResponse = await adapter.Invoke(CreateTabSubmitActivity(new TaskModuleRequest()
+            {
+                TabEntityContext = new TabEntityContext() { TabEntityId = "/Cards/TabModule" },
+                Data = card.GetElements<AdaptiveSubmitAction>().Single(a => a.Id == "OnIncrement").Data,
+            }));
+            tabResponse = ObjectPath.MapValueTo<TabResponse>(invokeResponse.Body);
+            Assert.IsNotNull(tabResponse);
+            Assert.IsNotNull(tabResponse.Tab);
+            Assert.AreEqual("continue", tabResponse.Tab.Type);
+            Assert.IsNotNull(tabResponse.Tab.Value);
+            card = ObjectPath.MapValueTo<AdaptiveCard>(tabResponse.Tab.Value.Cards.Single().Card);
+
+            card.AssertTextBlock("Counter: 1")
+                .AssertHasNoRefresh()
+                .AssertHasSession()
+                .AssertHasOnlySubmitActions();
+        }
     }
 }
