788efd0f8d33e21e2074348b83979d14a759398b
Denis Voituron <dvoituron@microsoft.com>
2023-01-19 14:43:09 +0100

    Refactorization of the Blazor Server and Blazor WebAssembly (Wasm) projects

---
+++ source/samples/BlazorClient/Server/Pages/Error.cshtml.cs
@@ -0,0 +1,30 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+namespace CrazorBlazorClientDemo.Server.Pages
+{
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Mvc.RazorPages;
+    using System.Diagnostics;
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    [IgnoreAntiforgeryToken]
+    public class ErrorModel : PageModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+
+        private readonly ILogger<ErrorModel> _logger;
+
+        public ErrorModel(ILogger<ErrorModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
+        }
+    }
+}
---
+++ source/samples/BlazorClient/Wasm/Program.cs
@@ -0,0 +1,43 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Crazor.Blazor;
+using Crazor;
+
+using CrazorBlazorClientDemo.Wasm;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Bot.Builder;
+using CrazorBlazorClientDemo.Wasm.Helpers;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+// ---- <CRAZOR>
+
+// register AppSettings.json file from wwwroot folder
+var http = new HttpClient()
+{
+    BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
+};
+
+builder.Services.AddScoped(sp => http);
+
+using var response = await http.GetAsync("appsettings.json");
+using var stream = await response.Content.ReadAsStreamAsync();
+
+builder.Configuration.AddJsonStream(stream);
+
+// register blob storage for state management
+builder.Services.AddCrazor("CrazorBlazorClientDemo.Shared");
+builder.Services.AddCrazorBlazor();
+
+// If you're using the StateController.cs file, to save and load state.
+// builder.Services.AddScoped<IStorage, RemoteStorage>();
+
+// ---- </CRAZOR>
+
+await builder.Build().RunAsync();
