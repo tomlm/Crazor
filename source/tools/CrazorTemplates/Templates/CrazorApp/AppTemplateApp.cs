d31b8611026be297e65524b5321715bd9d98d221
Tom Laird-McConnell <thermous@iciclecreek.com>
2024-06-16 11:38:31 -0700

    add new templates

---
+++ source/tools/CrazorTemplates/Templates/CrazorApp/AppTemplateApp.cs
@@ -0,0 +1,12 @@
+﻿using Crazor;
+using Crazor.Attributes;
+
+namespace Crazor.ClassLibrary1.Cards.AppTemplate
+{
+    public class AppTemplateApp : CardApp
+    {
+        public AppTemplateApp(CardAppContext context) : base(context)
+        {
+        }
+    }
+}
---
+++ source/tools/CrazorTemplates/Templates/CrazorViewWithModel/NewViewModel.cs
@@ -0,0 +1,8 @@
+﻿
+namespace Crazor.ClassLibrary1.Cards
+{
+    public class NewViewModel
+    {
+        public int Counter { get; set; }
+    }
+}
